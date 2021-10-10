4786093b5a03eda9b8667de44a6ab8478b9186fd
pthao180 <lnphuongthao.k2@gmail.com>
2021-10-10 23:15:33 +0700

    tuan05

---
+++ src/com/thaolnp20051100053/tuan05/lab5bai1.java
@@ -0,0 +1,27 @@
+package com.thaolnp20051100053.tuan05;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.concurrent.atomic.DoubleAccumulator;
+
+public class lab5bai1 {
+    public static void main(String[] args) {
+        ArrayList<Double> soThuc = new ArrayList<Double>();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            Double x = scanner.nextDouble();
+            soThuc.add(x);
+            System.out.println("Nhập thêm Y/N ? ");
+            scanner.nextLine();
+            if (scanner.nextLine().equals("n"))
+                break;
+        }
+        double tong = 0;
+        for (Double x : soThuc) {
+            System.out.print(x + " ");
+            tong = tong + x;
+        }
+        System.out.println("\n Tổng = " + tong);
+        scanner.close();
+    }
+}
